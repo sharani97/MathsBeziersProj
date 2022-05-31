21e5ed2494b5143bd6017580d54676169006be59
Shaadit <56261900+Shaadit@users.noreply.github.com>
2022-05-31 13:36:15 +0200

    push curve

--- Assets/Scripts/Casteljau.cs
+++ Assets/Scripts/Casteljau.cs
@@ -10,80 +10,74 @@ public class Casteljau : MonoBehaviour
     //PUBLIC
     public List<Transform> ControlPointsTransform;
     public Color MaterialColor;
-    public int stepLength = 5;
+    public float s = 0.2f;
+    public GameObject prefab;
 
     //PRIVATE
     private List<Vector3> bezierCurvePoints;
     private List<Vector3> curvePoints;
     private List<Vector3> controlPointsVector;
-    private float step;
     private float t;
 
     #endregion Members
 
     void Start()
     {
-        step = 1 / stepLength;
         controlPointsVector = new List<Vector3>();
         bezierCurvePoints = new List<Vector3>();
-
-        foreach(var c in ControlPointsTransform)
-        {
-            controlPointsVector.Add(c.position);
-        }
-
-
-        for (t = 0; t<=1; t+=step)
-        {
-            Vector3 vec = getBezierPoint(t, controlPointsVector);
-            Debug.Log(vec);
-            bezierCurvePoints.Add(vec);
-        }
-        
     }
-
-
-    private Vector3 getBezierPoint(float time, List<Vector3> controlPointsList)
-    {
-        var degree = controlPointsList.Count - 1;
-
-        curvePoints = new List<Vector3>();
-
-        for (int etape = degree; etape >= 0; etape--)
-        {
-            if (etape == degree)
-            {
-                for (int k = 0; k <= degree; k++)
-                {
-                    curvePoints.Add(controlPointsList[k]);
-                }
-                continue;
-            }
-            
-
-            int last = curvePoints.Count;            //our last index in the last list
-            int lvl = etape + 2;                     //our level is count - 1 so we need to add 2 to be on +1
-            int index = last - lvl;                  //find our current index
-
-            for (int point = 0; point <= etape; point++)
-            {
-                Vector3 p = (1 - time) * curvePoints[index] + time * curvePoints[index + 1]; ;
-                curvePoints.Add(p);
-                ++index;
-            }
-
-        }
-
-        int lastElmnt = curvePoints.Count - 1;
-        return curvePoints[lastElmnt];
-    }
-
 
     
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("z"))
+        {
+            TryDrawCurve();
+        }
     }
-    
+
+
+
+    public void TryDrawCurve()
+    {
+        for (t = 0; t <= 1; t += s)
+        {
+            Vector3 vec = getBezierPoint(t, ControlPointsTransform);
+            bezierCurvePoints.Add(vec);
+            GameObject pointBez = Instantiate(prefab, vec, Quaternion.identity);
+        }
+    }
+
+    private Vector3 getBezierPoint(float time, List<Transform> controlPoints)
+    {
+        var degree = controlPoints.Count - 1;
+
+        List<Vector3> bezierPoints = new List<Vector3>();
+        for (int level = degree; level >= 0; level--)
+        {
+            if (level == degree)
+            {
+                for (int i = 0; i <= degree; i++)
+                {
+                    bezierPoints.Add(controlPoints[i].position);
+                }
+                continue;
+            }
+
+            int lastIdx = bezierPoints.Count;
+            int levelIdx = level + 2;
+            int idx = lastIdx - levelIdx;
+            for (int i = 0; i <= level; i++)
+            {
+                Vector3 pi = (1 - time) * bezierPoints[idx] + time * bezierPoints[idx + 1];
+                bezierPoints.Add(pi);
+                ++idx;
+            }
+        }
+
+        int lastElmnt = bezierPoints.Count - 1;
+        return bezierPoints[lastElmnt];
+    }
+
 }
